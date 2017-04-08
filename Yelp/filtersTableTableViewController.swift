c4e10583bb2672bd3ca24092744bc1398d209685
Krishna Alex <krishna.aromal@gmail.com>
2017-04-08 15:39:34 -0700

    Filter page functionality changes

--- Yelp/filtersTableTableViewController.swift
+++ Yelp/filtersTableTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class filtersTableTableViewController: UITableViewController {
     
-      var sectionSet = [ ["caption": "Deals",
+      var sectionSet = [ ["caption": "Deals Offered",
                         "key": "deals",
                         "type": "Switch"],
                        ["caption": "Distance",
@@ -21,13 +21,15 @@ class filtersTableTableViewController: UITableViewController {
                         "type": "List"],
                        ["caption": "Category",
                         "key": "category",
-                        "type": "List"]]
+                        "type": "Switch"]]
     
 
     @IBOutlet var filtersTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        filtersTableView.dataSource = self
+
         
         filtersTableView.estimatedRowHeight = 30
         filtersTableView.rowHeight = UITableViewAutomaticDimension
@@ -77,6 +79,13 @@ class filtersTableTableViewController: UITableViewController {
         //return self.settingSections[section]
         
     }
+    
+    override func tableView(_ filtersTableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return tableView.sectionHeaderHeight * 2
+        }
+        return tableView.sectionHeaderHeight
+    }
 
     override func tableView(_ filtersTableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
@@ -123,7 +132,7 @@ class filtersTableTableViewController: UITableViewController {
             case 3:
                 options = categoryOptions()
                 let optionValue = options[indexPath.row]
-                ListCell.listLabel?.text = optionValue["code"]
+                switchCell.switchLabel?.text = optionValue["code"]
             default:
                 break
         }
@@ -154,17 +163,60 @@ class filtersTableTableViewController: UITableViewController {
     }
 
     
-    
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
+    /*override func tableView(_ filtersTableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if (cell.responds(to: #selector(getter: UIView.tintColor))) {
+            let cornerRadius: CGFloat = 5
+            cell.backgroundColor = UIColor.clear
+            let layer: CAShapeLayer  = CAShapeLayer()
+            let pathRef: CGMutablePath  = CGMutablePath()
+            let bounds: CGRect  = cell.bounds.insetBy(dx: 10, dy: 0)
+            var addLine: Bool  = false
+            if (indexPath.row == 0 && indexPath.row == tableView.numberOfRows(inSection: indexPath.section)-1) {
+                pathRef.__addRoundedRect(transform: nil, rect: bounds, cornerWidth: cornerRadius, cornerHeight: cornerRadius)
+            } else if (indexPath.row == 0) {
+                pathRef.move(to: CGPoint(x:bounds.minX,y:bounds.maxY))
+                pathRef.addArc(tangent1End: CGPoint(x:bounds.minX,y:bounds.minY), tangent2End: CGPoint(x:bounds.midX,y:bounds.minY), radius: cornerRadius)
+                
+                pathRef.addArc(tangent1End: CGPoint(x:bounds.maxX,y:bounds.minY), tangent2End: CGPoint(x:bounds.maxX,y:bounds.midY), radius: cornerRadius)
+                pathRef.addLine(to: CGPoint(x:bounds.maxX,y:bounds.maxY))
+                addLine = true;
+            } else if (indexPath.row == tableView.numberOfRows(inSection: indexPath.section)-1) {
+                
+                pathRef.move(to: CGPoint(x:bounds.minX,y:bounds.minY))
+                pathRef.addArc(tangent1End: CGPoint(x:bounds.minX,y:bounds.maxY), tangent2End: CGPoint(x:bounds.midX,y:bounds.maxY), radius: cornerRadius)
+                
+                pathRef.addArc(tangent1End: CGPoint(x:bounds.maxX,y:bounds.maxY), tangent2End: CGPoint(x:bounds.maxX,y:bounds.midY), radius: cornerRadius)
+                pathRef.addLine(to: CGPoint(x:bounds.maxX,y:bounds.minY))
+                
+            } else {
+                pathRef.addRect(bounds)
+                addLine = true
+            }
+            layer.path = pathRef
+            //CFRelease(pathRef)
+            //set the border color
+            //layer.strokeColor = UIColor.lightGray.cgColor
+            layer.strokeColor = UIColor.black.cgColor
+            //set the border width
+            layer.lineWidth = 1
+            layer.fillColor = UIColor(white: 1, alpha: 1.0).cgColor
+            
+            
+            if (addLine == true) {
+                let lineLayer: CALayer = CALayer()
+                let lineHeight: CGFloat  = (1 / UIScreen.main.scale)
+                lineLayer.frame = CGRect(x:bounds.minX, y:bounds.size.height-lineHeight, width:bounds.size.width, height:lineHeight)
+                lineLayer.backgroundColor = tableView.separatorColor!.cgColor
+                layer.addSublayer(lineLayer)
+            }
+            
+            let testView: UIView = UIView(frame:bounds)
+            testView.layer.insertSublayer(layer, at: 0)
+            testView.backgroundColor = UIColor.clear
+            cell.backgroundView = testView
+        }
+        
+    } */
 
     /*
     // Override to support conditional editing of the table view.
