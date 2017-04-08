f0f746c658e230bb8f82865dc80f1f1c69979d50
Krishna Alex <krishna.aromal@gmail.com>
2017-04-08 02:57:31 -0700

    Created filter page Added Sections and options in Filter

---
+++ Yelp/SwitchCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchCell.swift
+//  Yelp
+//
+//  Created by Krishna Alex on 4/8/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class SwitchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
